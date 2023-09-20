643405e8db1a4fa225dc11e7a57659d9e8ad9f45
dipbhi <dipen.bhikadya@gmail.com>
2023-09-20 11:39:46 +0530

    meta description update

--- app.vue
+++ app.vue
@@ -1,50 +1,50 @@
 <template>
-  <NuxtLayout>
-    <template v-if="state.criticalDataLoading">
-      <v-main>
-        <v-container fluid>
-          <LoadingIndicatorRow></LoadingIndicatorRow>
-        </v-container>
-      </v-main>
-    </template>
-    <NuxtPage v-else />
-  </NuxtLayout>
+    <NuxtLayout>
+        <template v-if="state.criticalDataLoading">
+            <v-main>
+                <v-container fluid>
+                    <LoadingIndicatorRow></LoadingIndicatorRow>
+                </v-container>
+            </v-main>
+        </template>
+        <NuxtPage v-else />
+    </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 useHead({
-  title: "Twilio Viewer",
-  meta: [
-    { name: "description", content: "Easy viewing of Twilio accounts, subaccounts, and related resources (such as message, and voice call logs)." },
-  ],
-  link: [
-    { rel: "icon", type: "image/png", href: "/icon.png" },
-  ],
+    title: "Twilio Viewer",
+    meta: [
+        { name: "description", content: "Easy viewing of Twilio accounts, subaccounts, and related resources (such as message & voice call logs, alerts, events, and billing)." },
+    ],
+    link: [
+        { rel: "icon", type: "image/png", href: "/icon.png" },
+    ],
 });
 
 // critical data points for all the routes needs to be loaded upfront
 const store = useMainStore();
 
 const state = ref({
-  criticalDataLoading: true,
+    criticalDataLoading: true,
 });
 
 store.savedConnectionsFill().then(() => {
-  state.value.criticalDataLoading = false;
+    state.value.criticalDataLoading = false;
 });
 </script>
 
 <style>
 table,
 table .v-list-item-subtitle {
-  font-size: .85rem;
+    font-size: .85rem;
 }
 
 table .v-list-item-title {
-  font-size: .9rem;
+    font-size: .9rem;
 }
 
 .v-autocomplete__selection {
-  max-width: 100%!important;
+    max-width: 100% !important;
 }
 </style>
